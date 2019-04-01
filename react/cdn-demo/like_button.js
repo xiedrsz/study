fd3b12c2eab399f79a51cdb5e93f6d62b01f10c7
xiedrsz <1805713222@qq.com>
2019-04-01 15:45:47 +0800

    react学习

---
+++ react/cdn-demo/like_button.js
@@ -0,0 +1,5 @@
+// ... the starter code you pasted ...
+var e = React.createElement;
+
+var domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
